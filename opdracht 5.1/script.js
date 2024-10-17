66d25b8ddd9b4dbe4a6e8375f877c844b3467c83
MarcellinoDw <9026865@student.zadkine.nl>
2024-10-17 11:45:26 +0200

    Add files via upload

---
+++ opdracht 5.1/script.js
@@ -0,0 +1,8 @@
+// Selecteer het h1 element op basis van de ID
+const titelElement = document.getElementById("titel");
+
+// Pas de kleur aan van het element
+titelElement.style.color = "black";
+
+// Pas de tekst aan van het element
+titelElement.innerText = "Kop";
