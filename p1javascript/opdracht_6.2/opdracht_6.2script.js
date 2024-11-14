77eb91683d3e1117a3e647c774d2ff167c833c26
MarcellinoDw <9026865@student.zadkine.nl>
2024-11-14 11:00:03 +0100

    Add files via upload

---
+++ p1javascript/opdracht_6.2/opdracht_6.2script.js
@@ -0,0 +1,10 @@
+function kleur_aanpassen() {
+    // Selecteer het h1 element
+    const h1Element = document.getElementById('hoofdtekst');
+    
+    // Verander de kleur
+    h1Element.style.color = 'blue';
+    
+    // Verander de tekst
+    h1Element.textContent = 'Inhoud aangepast!';
+}
